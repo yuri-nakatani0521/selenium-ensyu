1130297bb228a3bc4a7723eb9240232f7a1dc31d
yuri-nakatani0521 <yuri.nakatani@rakus-partners.co.jp>
2021-11-15 11:39:37 +0900

    first commit

---
+++ src/selenium/SeleniumTest.java
@@ -0,0 +1,29 @@
+package selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumTest {
+
+	public static void main(String[] args) {
+		try {
+			System.setProperty("webdriver.chrome.driver", "./exe/chromedriver");
+			WebDriver driver = new ChromeDriver();//ChromeDriverの作成
+			driver.get("http://www.google.com/xhtml"); //get()でブラウザ起動
+			Thread.sleep(5000);//処理を停止(動作確認の為)
+			//ブラウザの要素を指定　メソッド名がわかりやすいですよね。
+			WebElement searchBox = driver.findElement(By.name("q"));
+			searchBox.sendKeys("ChromeDriver");
+			searchBox.submit();
+			Thread.sleep(5000);
+			driver.quit();	
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+
+	}
+
+}
